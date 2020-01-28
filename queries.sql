3419aaac478b729986a3de676316f8897653a8cf
tamzidpeace <tamjedpeace@gmail.com>
2020-01-28 19:50:07 +0600

    first commit

---
+++ queries.sql
@@ -0,0 +1,3 @@
+12. select * from nobel_win where YEAR = 1970;
+13. select * from nobel_win where YEAR = 1971 and SUBJECT='Literature';
+14. select YEAR, SUBJECT from nobel_win where WINNER='Dennis Gabor';
